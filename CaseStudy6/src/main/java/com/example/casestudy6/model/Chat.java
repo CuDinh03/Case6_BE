bd5a05b426a97882c66b22d0b9fed063df936f43
TranPhong04 <phongy3ulinh@gmail.com>
2023-01-04 18:07:21 +0700

    data base

---
+++ CaseStudy6/src/main/java/com/example/casestudy6/model/Chat.java
@@ -0,0 +1,27 @@
+package com.example.casestudy6.model;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Entity
+public class Chat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String contentChat;
+    @ManyToMany
+    private Set<Account> account;
+    private int status;
+
+    public Chat() {
+    }
+
+    public Chat(Long id, String contentChat, Set<Account> account, int status) {
+        this.id = id;
+        this.contentChat = contentChat;
+        this.account = account;
+        this.status = status;
+    }
+}
