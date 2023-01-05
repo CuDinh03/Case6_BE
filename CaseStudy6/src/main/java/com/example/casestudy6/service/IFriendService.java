2a8b2f0e02a7dfc1f86c9b9584b4e67d2d9f44fb
Sonhr1403 <sonhr1403@gmail.com>
2023-01-05 16:13:34 +0700

    xóa bớt phần thừa để tránh xung đột

--- CaseStudy6/src/main/java/com/example/casestudy6/service/IFriendService.java
+++
@@ -1,7 +0,0 @@
-package com.example.casestudy6.service;
-
-import com.example.casestudy6.model.Friends;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface IFriendService extends PagingAndSortingRepository< Friends, Long> {
-}
