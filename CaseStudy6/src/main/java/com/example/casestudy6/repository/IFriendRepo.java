f42948e3c373f63ae706023b89335d4f84292d0b
TranPhong04 <phongy3ulinh@gmail.com>
2023-01-05 15:18:59 +0700

    account them role

--- CaseStudy6/src/main/java/com/example/casestudy6/repository/IFriendRepo.java
+++ CaseStudy6/src/main/java/com/example/casestudy6/repository/IFriendRepo.java
@@ -10,4 +10,6 @@ import java.util.ArrayList;
 public interface IFriendRepo extends PagingAndSortingRepository<Friends,Long> {
     @Query(nativeQuery = true,value = "SELECT * FROM Friends where Friends.account1 = :account1 and Friends.status  = 2")
     ArrayList<Friends> getAllFriends(@Param("account1") String account1);
+    @Query(nativeQuery = true,value = " UPDATE friends SET status = 3 WHERE Friends.account1 = :account1 and Friends.account2_id = :account2_id")
+    void blockFriend(@Param("account1") String account1, @Param("account2_id") String account2_id);
 }
