6791bc35c23a079d60a794eb67b07c8181234141
haiyennguyen23 <haiyennguyen05102k3@gmail.com>
2023-01-06 08:13:09 +0700

    update

--- CaseStudy6/src/main/java/com/example/casestudy6/service/impl/FriendService.java
+++ CaseStudy6/src/main/java/com/example/casestudy6/service/impl/FriendService.java
@@ -2,5 +2,5 @@ package com.example.casestudy6.service.impl;
 
 import com.example.casestudy6.service.IFriendService;
 
-public class FriendService implements IFriendService {
+public class FriendService extends IFriendService {
 }
