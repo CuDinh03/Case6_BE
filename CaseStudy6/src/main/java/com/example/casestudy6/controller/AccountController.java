9a7c7c3c04561807115ae0f0a21797866326cace
phamvanhanh6720 <phamvanhanh05808@gmail.com>
2023-01-11 17:55:07 +0700

    dm phong

--- CaseStudy6/src/main/java/com/example/casestudy6/controller/AccountController.java
+++ CaseStudy6/src/main/java/com/example/casestudy6/controller/AccountController.java
@@ -1,2 +1,48 @@
-package com.example.casestudy6.controller;public class AccountController {
+package com.example.casestudy6.controller;
+
+import com.example.casestudy6.model.Account;
+import com.example.casestudy6.model.dto.ChangePassword;
+import com.example.casestudy6.service.impl.AccountService;
+import com.example.casestudy6.service.impl.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Validated
+@RestController
+@CrossOrigin("*")
+public class AccountController {
+    @Autowired
+    AccountService accountService;
+    @Autowired
+    RoleService roleService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    @PutMapping("/change-password")
+    public ResponseEntity<Account> changePassword(@RequestBody ChangePassword changePassword) {
+        UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Account account = accountService.findByUserName(userDetails.getUsername());
+        String newPassword;
+        String oldPassword = changePassword.getOldPassword();
+        if (oldPassword.equals(account.getPassword())) {
+            if (changePassword.getNewPassword().equals(changePassword.getConfirmNewPassword())) {
+                newPassword = changePassword.getNewPassword();
+            } else {
+                return new ResponseEntity<>(HttpStatus.OK);
+            }
+        } else {
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        account.setPassword(newPassword);
+        accountService.save(account);
+        return new ResponseEntity<>(account, HttpStatus.OK);
+    }
 }
