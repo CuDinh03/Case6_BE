6e5f13d7dbb58b81f991f5350bcf69ad249a0424
phamvanhanh6720 <phamvanhanh05808@gmail.com>
2023-01-13 14:24:10 +0700

    done chuc nang login khi bi block tai khoan

--- CaseStudy6/src/main/java/com/example/casestudy6/controller/LoginController.java
+++ CaseStudy6/src/main/java/com/example/casestudy6/controller/LoginController.java
@@ -1,7 +1,6 @@
 package com.example.casestudy6.controller;
 
 import com.example.casestudy6.model.Account;
-import com.example.casestudy6.model.dto.AccLogin;
 import com.example.casestudy6.model.dto.JwtResponse;
 import com.example.casestudy6.service.impl.AccountService;
 import com.example.casestudy6.service.impl.JwtService;
@@ -14,12 +13,15 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Validated
 @RestController
@@ -39,19 +41,46 @@ public class LoginController {
 
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody Account account) {
-        if (!accountService.isRegister(account)) {
-            return new ResponseEntity<>("1", HttpStatus.NOT_FOUND);
+    public ResponseEntity<List<Object>> login(@RequestBody Account account) {
+        List<Object> result = new ArrayList<>();
+        JwtResponse jwtResponse;
+        if (accountService.checkLogin(account) == 1) {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(account.getUserName(), account.getPassword()));
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            String token = jwtService.createToken(authentication);
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            Account currentUser = accountService.findByUserName(account.getUserName());
+            jwtResponse = new JwtResponse();
+            jwtResponse.setToken(token);
+            jwtResponse.setId(currentUser.getId());
+            jwtResponse.setUserName(currentUser.getUserName());
+            jwtResponse.setEmail( currentUser.getEmail());
+            jwtResponse.setPhoneNumber(currentUser.getPhoneNumber());
+            jwtResponse.setImg(currentUser.getImg());
+            jwtResponse.setFirstName(currentUser.getFirstName());
+            jwtResponse.setLastName(currentUser.getLastName());
+            jwtResponse.setAddress(currentUser.getAddress());
+            jwtResponse.setGender(currentUser.getGender());
+            jwtResponse.setBirthDay(currentUser.getBirthDay());
+            jwtResponse.setRoles(userDetails.getAuthorities());
+            result.add(1);
+            result.add(jwtResponse);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+//            return ResponseEntity.ok(new JwtResponse(token, currentUser.getId(), currentUser.getUserName(), currentUser.getEmail(), currentUser.getPhoneNumber(), currentUser.getImg(), currentUser.getFirstName(), currentUser.getLastName(), currentUser.getAddress(), currentUser.getGender(), currentUser.getBirthDay(), userDetails.getAuthorities()));
+        } else if (accountService.checkLogin(account) == 2) {
+            //tai khoan bi block
+            result.add(2);
+            result.add(jwtService);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } else {
+            //sai tai khoan hoac mat khau
+            result.add(3);
+            result.add(jwtService);
+            return new ResponseEntity<>(result, HttpStatus.OK);
         }
-        Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(account.getUserName(), account.getPassword()));
-
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        String token = jwtService.createToken(authentication);
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        Account currentUser = accountService.findByUserName(account.getUserName());
-        return ResponseEntity.ok(new JwtResponse(token, currentUser.getId(), currentUser.getUserName(), currentUser.getEmail(), currentUser.getPhoneNumber(),currentUser.getImg(), currentUser.getFirstName(), currentUser.getLastName(), currentUser.getAddress(), currentUser.getGender(),currentUser.getBirthDay() ,userDetails.getAuthorities()));
     }
 }
 
--- CaseStudy6/src/main/java/com/example/casestudy6/service/IAccountService.java
+++ CaseStudy6/src/main/java/com/example/casestudy6/service/IAccountService.java
@@ -1,7 +1,6 @@
 package com.example.casestudy6.service;
 
 import com.example.casestudy6.model.Account;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.Optional;
@@ -16,7 +15,7 @@ public interface IAccountService extends UserDetailsService {
 
     Optional<Account> findById(Long id);
 
-    boolean checkLogin(Account account);
+    long checkLogin(Account account);
 
     boolean isRegister(Account account);
 
