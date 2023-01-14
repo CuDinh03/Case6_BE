b60d0d7a722fa13e2c1bc766eede6e5280f56d9a
CuDinh03 <dinhhuycu0305@gmail.com>
2023-01-15 00:54:20 +0700

    ok

--- CaseStudy6/src/main/java/com/example/casestudy6/repository/ICommentRepo.java
+++ CaseStudy6/src/main/java/com/example/casestudy6/repository/ICommentRepo.java
@@ -1,9 +1,9 @@
 package com.example.casestudy6.repository;
 
 import com.example.casestudy6.model.Comment;
-import com.example.casestudy6.model.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -11,5 +11,8 @@ public interface ICommentRepo extends JpaRepository<Comment, Long> {
 
     @Query(value = "select * from  where (status = 1 or status = 2) order by post_day desc ", nativeQuery = true)
     List<Comment> findAll();
+
+    @Query(value = "insert into status_comment value (:status_id, :comment_id)", nativeQuery = true)
+    void saveComment(@Param("status_id") Long status_id, @Param("comment_id") Long comment_id);
 
 }
