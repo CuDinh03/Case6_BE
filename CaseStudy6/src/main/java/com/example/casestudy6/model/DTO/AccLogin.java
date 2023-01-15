c61b367d245220ec929e60eb74e2ce2692958375
Sonhr1403 <sonhr1403@gmail.com>
2023-01-16 00:39:15 +0700

    update comment

--- CaseStudy6/src/main/java/com/example/casestudy6/model/DTO/AccLogin.java
+++ CaseStudy6/src/main/java/com/example/casestudy6/model/DTO/AccLogin.java
@@ -2,9 +2,11 @@ package com.example.casestudy6.model.DTO;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Component
 public class AccLogin {
     private String userName;
     private String email;
