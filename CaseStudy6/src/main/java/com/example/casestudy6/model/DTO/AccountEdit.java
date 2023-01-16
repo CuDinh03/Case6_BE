19cd9609361c447ac0d1501b04808ec401d18a14
TranPhong04 <phongy3ulinh@gmail.com>
2023-01-16 11:01:24 +0700

    okedit

---
+++ CaseStudy6/src/main/java/com/example/casestudy6/model/DTO/AccountEdit.java
@@ -0,0 +1,93 @@
+package com.example.casestudy6.model.dto;
+
+import java.util.Date;
+
+public class AccountEdit {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Date birthDay;
+    private String gender;
+    private String address;
+    private String phoneNumber;
+    private int status;
+
+    public AccountEdit(Long id, String username, String firstName, String lastName, Date birthDay, String gender, String address, String phoneNumber, int status) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDay = birthDay;
+        this.gender = gender;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
